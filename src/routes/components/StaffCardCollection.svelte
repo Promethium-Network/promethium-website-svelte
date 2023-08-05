2411806f3a667eadc4b90fda7eb5d29cf73606bd
Augustus Bunce <augustusbunce@gmail.com>
2023-08-05 02:56:48 +0100

    It has been a long night
    I need to sleep, butttt who the fuck wants to sleep. Plus i have a train to catch in like 3 hours from euston, that is going to take me like TWO FUCKING HOURS TO GET TO INSTEAD OF 30 MINUTES BECAUSE THE FUCKING ELIZABETH LINE DOESN'T OPERATE OVERNIGHT.

--- src/routes/components/StaffCardCollection.svelte
+++ src/routes/components/StaffCardCollection.svelte
@@ -1,8 +1,7 @@
 <script>
 	import StaffCard from './StaffCard.svelte';
 
-    import { Spinner } from 'flowbite-svelte';
-
+	import { Spinner } from 'flowbite-svelte';
 
 	let promise = fetch(
 		'https://promethium-network-website-db.fly.dev/api/collections/staff/records/?filter=(active=true)&sort=+discord_username'
@@ -10,11 +9,35 @@
 </script>
 
 <div>
-    {#await promise}
-        <Spinner color='purple' size={8} class='text-center'/>
-    {:then data} 
-        
-    {:catch error}
-        {alert('DB CONNECTION LOST')}
-    {/await}
+	<div>
+		<h1
+			class="text-center font-semibold whitespace-nowrap text-5xl dark:text-white"
+		>
+			Staff
+		</h1>
+	</div>
+
+	<div class="container mx-auto py-10">
+		{#await promise}
+			<Spinner color="purple" />
+		{:then data}
+			<div class="flex justify-between justify-items-center">
+				<StaffCard
+					name={data.items[1].name}
+					role={data.items[1].role}
+					bio={data.items[1].bio}
+					profilePicUrl={data.items[1].profile_pic_url}
+				/>
+				<StaffCard
+					name={data.items[0].name}
+					role={data.items[0].role}
+					bio={data.items[0].bio}
+					profilePicUrl={data.items[0].profile_pic_url}
+				/>
+			</div>
+		{:catch error}
+			{console.log(error)}
+			{alert('ERROR PLEASE CONTACT GUS')}
+		{/await}
+	</div>
 </div>
